f4b39614a3fd32a8d2bb992ce27c2849de95821f
sdausell <dausell28@hotmail.com>
2022-02-20 21:57:13 +0100

    Update HelloWorld.java

--- project/src/main/java/HelloWorld.java
+++ project/src/main/java/HelloWorld.java
@@ -1,12 +1,3 @@
-/**
- * Project Name    : sonarqube-example
- * Developer       : Osanda Deshan
- * Version         : 1.0.0
- * Date            : 8/8/2019
- * Time            : 4:28 PM
- * Description     :
- **/
-
 
 public class HelloWorld {
 
