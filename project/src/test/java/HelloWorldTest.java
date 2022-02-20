a1d45d71f7955c7230e749f2afba2c37369da8c6
sdausell <dausell28@hotmail.com>
2022-02-20 21:57:42 +0100

    Update HelloWorldTest.java

--- project/src/test/java/HelloWorldTest.java
+++ project/src/test/java/HelloWorldTest.java
@@ -1,15 +1,5 @@
 import org.junit.Test;
 
-/**
- * Project Name    : sonarqube-example
- * Developer       : Osanda Deshan
- * Version         : 1.0.0
- * Date            : 8/9/2019
- * Time            : 8:59 AM
- * Description     :
- **/
-
-
 public class HelloWorldTest {
 
     @Test
